c8c4d3682f020510596aad2d220649d11c0a0196
Roman Shevyakov <rs.8@bk.ru>
2024-03-07 16:01:56 +0500

    feat: set validation on

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.PORT || 4000);
 }
 bootstrap();
